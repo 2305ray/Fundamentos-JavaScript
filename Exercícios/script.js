c7781094ebddd855bb91f1272868cbfd97919f9e
rayssa garcia <rayssagarcia035@gmail.com>
2024-09-19 14:48:25 -0300

    hora do dia finalizada

--- Exercícios/script.js
+++ Exercícios/script.js
@@ -3,21 +3,31 @@ function carregar() {
     var msg = document.getElementById('msg');
     var img = document.getElementById('img');
     var data = new Date();
-    //var hora = data.getHours();
-    var hora = 19
+    var hora = data.getHours();
+    var min = data.getMinutes()
+    var sec = data.getSeconds()
+    
+    // Adicionando formatação para dois dígitos
+    min = min < 10 ? '0' + min : min;
+    sec = sec < 10 ? '0' + sec : sec;
 
-    msg.innerHTML = `Agora são ${hora} horas.`;
+    msg.innerHTML = `Agora são ${hora}:${min}:${sec}`
+
+   
 
     if (hora >= 0 && hora < 12) {
-        img.src="manha1.png";
+        img.src="manhã.jpg";
+
         // Bom dia!
     } else if (hora >= 12 && hora < 18) {
-        img.src = 'tarde1.png';
+        img.src = 'tarde.jpg';
         document.body.style.background = '#D2691E'
+         document.body.style.transitionDuration = "3s"
         // Boa tarde!
     } else {
-        img.src = 'noite1.png';
+        img.src = 'noite.jpg';
         document.body.style.background = "#515154"
+         document.body.style.transitionDuration = "3s"
         // Boa noite!
     }
 }
