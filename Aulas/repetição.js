6a67b79e3ef584fb61313ac1786ef83fc53455fa
rayssa garcia <rayssagarcia035@gmail.com>
2024-09-23 10:03:10 -0300

    aula 1 (repetição)

---
+++ Aulas/repetição.js
@@ -0,0 +1,24 @@
+// estrutura de repetição com teste no início
+
+var c = 1
+while(c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+}
+
+/* Ezemplo  de como seria sem o while
+Console.log('tudo bem?')
+Console.log('tudo bem?')
+Console.log('tudo bem?')
+Console.log('tudo bem?')
+Console.log('tudo bem?')
+*/
+
+
+// estrutura de repetição com teste no final
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 10)
