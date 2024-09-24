2bb4d9169edd4d8dc417ced8d7b05a3e08706049
rayssa garcia <rayssagarcia035@gmail.com>
2024-09-24 14:12:15 -0300

    parâmetro

---
+++ Aulas/parâmetro.js
@@ -0,0 +1,23 @@
+/* function parimp(n) {
+    if (n%2==0){
+        return 'Par!'
+    } else {
+        return'ímpar!'
+    }
+}
+let res = parimp(11)
+console.log(res)
+*/
+
+// se n passar o valor de um deles vai dar zero
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+console.log(soma(2,7))
+
+/* exemplo de função dentro de uma variável
+let v = function(x){
+return x*2
+}
+console.log(v(5))
+*/
